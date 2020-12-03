8999984d67666e3ed79e485e329f37db77788333
diskszk <ibalucky23@gmail.com>
2020-12-03 11:57:13 +0900

    add: add congrats components and test utils

---
+++ src/conmponents/Congrats.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Congrats = (props) => {
+  return (
+    <React.Fragment>
+      {props.success ? (
+        <div data-test="component-congrats">
+          <span data-test="congrats-message">
+            Congratulation! You gussed the word!
+          </span>
+        </div>
+      ) : (
+        <div data-test="component-congrats"></div>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default Congrats;
