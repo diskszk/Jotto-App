bb75a881b477167b47f6050daaffde97f06ba1e5
diskszk <ibalucky23@gmail.com>
2020-12-05 03:47:19 +0900

    start tests for GuessWords

--- src/tests/Congrats.test.jsx
+++ src/tests/Congrats.test.jsx
@@ -6,25 +6,25 @@ import CheckPropTypes from "check-prop-types";
 
 const defaultProps = { success: false };
 
-const setUp = (props = {}) => {
+const setup = (props = {}) => {
   const setupProps = { ...defaultProps, ...props };
   return shallow(<Congrats {...props} />);
 };
 
 test("renders without error", () => {
-  const wrapper = setUp();
+  const wrapper = setup();
   const component = findByTestAttr(wrapper, "component-congrats");
   expect(component.length).toBe(1);
 });
 
 test('renders no text when "success" props is false', () => {
-  const wrapper = setUp({ success: false });
+  const wrapper = setup({ success: false });
   const component = findByTestAttr(wrapper, "component-congrats");
   expect(component.text()).toBe("");
 });
 
 test('renders non-empty message when "success" props is true', () => {
-  const wrapper = setUp({ success: true });
+  const wrapper = setup({ success: true });
   const message = findByTestAttr(wrapper, "congrats-message");
   expect(message.text().length).not.toBe(0);
 });
--- test/testUtils.js
+++ test/testUtils.js
@@ -1,5 +1,3 @@
-import CheckPropTypes from "check-prop-types";
-
 export const findByTestAttr = (wrapper, value) => {
   return wrapper.find(`[data-test="${value}"]`);
 };
